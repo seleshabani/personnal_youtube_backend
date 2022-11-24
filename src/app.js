eb8759ad582ad79a3aabdccc2af4e8eded693b02
seleshabani <seleshabani4@gmail.com>
2022-11-24 10:03:45 +0100

    authentification : renvois du token en format json

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const { connectDb } = require('./models/dbConnection');
 const authRouteur = require('./routes/auth');
 const app = express();
 const PORT = process.env.PORT || 3500;
+const bodyParser = require('body-parser')
 const run = async ()=>{
     try {
         await connectDb(process.env.db);
@@ -12,7 +13,16 @@ const run = async ()=>{
     }
     
     app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
+   // app.use(express.urlencoded({ extended: true }));
+    app.use(bodyParser.urlencoded({ extended: false }))
+    app.use(bodyParser.json())
+    app.use(function(req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        next();
+    });
     app.use('/auth',authRouteur);
     app.listen(PORT,()=>{
         console.log("serveur demarré à l'adresse : http://localhost:"+PORT)
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -14,16 +14,18 @@ const publicKey = process.env.PUBLIC_KEY
 authRouteur.post('/subscribe',async (req,res)=>{
     let email = req.body.email;
     let googleId = req.body.googleId;
-    let password = req.body.password;
-    let verifuser = await User.findOne({email:email});
-    if(verifuser) return res.status(403).send('un utilisateur avec ce mail existe déjà');
-    const ashedpasword = await cryptPassword(password);
-    const user = await User.create({email:email,password:ashedpasword,googleId});
+    console.log(req.body);
+    //return res.send('ok')
+  //  let password = req.body.password;
+    let verifuser = await User.findOne({email:email}).exec();
+    if(verifuser) return res.status(403).json(false);
+   // const ashedpasword = await cryptPassword(password);
+    const user = await User.create({email:email,googleId:googleId});
     const token = jwt.sign({email}, privateKey, {algorithm: "RS256"});
 
     if (user) {
         res.status(201);
-        return res.send(token);
+        return res.json(token);
     }
     
     res.status(422);
@@ -32,23 +34,20 @@ authRouteur.post('/subscribe',async (req,res)=>{
 
 authRouteur.post('/login',async (req,res)=>{
     let email = req.body.email;
-    let password = req.body.password;
+   // let password = req.body.password;
     let user = await User.findOne({email:email});
-    if(!user) return res.status(403).send('aucun utilisateur avec ce mail n\'a été trouvé');
-    let isSamePassword = await comparePassword(password,user.password);
+    if(!user) return res.status(403).json(false);
+    //let isSamePassword = await comparePassword(password,user.password);
 
-    if(isSamePassword){
-        const token = jwt.sign({email}, privateKey, {algorithm: "RS256"})
-        res.status(200);
-        return res.send(token)
-    }
-    //console.log(password,user.password);
+   // if(isSamePassword){
+    const token = jwt.sign({email}, privateKey, {algorithm: "RS256"})
     res.status(200);
-    res.json('mot de passe incorrect');
+    return res.json(token)
+   // }
+    //console.log(password,user.password);
+    //res.status(200);
+   // res.json('mot de passe incorrect');
 
-})
-authRouteur.get('/protected',isUserLogedIn,(req,res)=>{
-    return res.status(200).send('hello');
 })
 
 module.exports = authRouteur;
