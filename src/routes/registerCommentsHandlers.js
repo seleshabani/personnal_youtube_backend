cfc96dc5e6293fc83308682c933f47548bda0aff
seleshabani <seleshabani4@gmail.com>
2022-12-06 17:21:56 +0100

    comments : gestion de la route (socket) pour ajouter une réponse à un commentaire

--- src/routes/registerCommentsHandlers.js
+++ src/routes/registerCommentsHandlers.js
@@ -2,6 +2,7 @@ const {Comment} = require('../models/comment');
 const {User} = require('../models/users');
 
 module.exports = (io, socket) => {
+
     const createComment = async (payload) => {
       // la récuperation de l'email du user via la req est temporaire (il doit être recuperé via le token au futur)
       const {contenue,videoId,userMail} = JSON.parse(payload)
@@ -17,7 +18,15 @@ module.exports = (io, socket) => {
       io.emit('comment:get',JSON.stringify(comments))
     }
   
+    const createCommentResponse = async(payload) =>{
+        const {contenue,token,videoId,email,parentId} = JSON.parse(payload);
+        const user = await User.find({email:email}).exec();
+        const comment = await Comment.create({content:contenue,videoId:videoId,author:user._id,parentId:parentId});
+        const comments = await Comment.find({videoId:videoId,parentId:parentId}).exec();
+        io.emit('comment:get:response',comments);
+      }
    // socket.on("comment:create", createComment);
     socket.on("comment:read", readComment);
     socket.on("comment:create",createComment);
+    socket.on("comment:create:response",createCommentResponse);
 }
